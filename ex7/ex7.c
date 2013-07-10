7b7ecfd72c0ce42412c276fd1ba3530ef42a5d0d
Pietro Rea <pietrorea@gmail.com>
2013-07-10 18:06:30 -0400

    Initial commit. Exercises 1-17

---
+++ ex7/ex7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int bugs = 100;
+	double bug_rate = 1.2;
+
+	printf("You have %d bugs at the imaginary rate of %f.\n",
+		bugs, bug_rate);
+
+	long universe_of_defects 1L * 1024L * 1024 * 1024L;
+	printf("The entire universe has %ld bugs.\n",
+		universe_of_defects);
+
+	double expected_bugs = bugs * bugs_rate;
+	printf()
+}
